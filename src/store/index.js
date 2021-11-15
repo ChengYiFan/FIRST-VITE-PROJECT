5bd6b0710902cd9657a6b8b4106779c5034b0674
chengyifan1 <chengyifan1@360.cn>
2021-11-15 19:40:56 +0800

    feat: vuex 状态 getters

--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,24 @@ import { createStore } from 'vuex'
 
 // Create a new store instance.
 const store = createStore({
-  state () {
-    return {
-      count: 0
-    }
+  state: {
+    count: 0,
+    todos: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false }
+    ]
   },
   mutations: {
     increment (state) {
       state.count++
+    }
+  },
+  getters: {
+    doneTodos: (state) => {
+      return state.todos.filter(todo => todo.done).length
+    },
+    getTodoById: (state) => (id) => {
+      return state.todos.find(todo => todo.id === id)
     }
   }
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,27 @@
-<template>this is home</template>
+<template>
+  this is home
+  <div>count: {{count}}</div>
+  <div>doneTodosCount: {{doneTodosCount}}</div>
+</template>
 <script>
+import { mapState } from 'vuex'
+
 export default {
   mounted() {
     this.increment()
   },
+  computed: {
+    ...mapState({
+      count: state => state.count
+    }),
+    doneTodosCount () {
+      return this.$store.getters.doneTodos
+      // this.$store.getters.getTodoById(2).length
+    }
+  },
   methods: {
     increment() {
       this.$store.commit('increment')
-      console.log(this.$store.state.count)
     }
   }
 }
